7f73721679962ded3f92de1dcd2cf87a3ab091d8
Shinichi Kato <skat@shinichis-air.lan>
2021-12-08 16:23:28 -0800

    added auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "amplifytestd9769a7f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
 }
--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,9 @@
 // @ts-check
 import { initSchema } from '@aws-amplify/datastore';
 import { schema } from './schema';
-
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 
 const { Todo } = initSchema(schema);
